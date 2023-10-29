5f4701d9178c2922526a1e27dad348400584d15f
Vladlen Popolitov <vladlenpopolitov@gmail.com>
2023-10-29 22:39:24 +0300

    Results added to PNG in results folder

--- plot/plot_lastfm_10.R
+++ plot/plot_lastfm_10.R
@@ -9,10 +9,13 @@ pgvector_hnsw <- resultsSorted[resultsSorted$Algorithm == 'pgvector_hnsw',]
 pgvector_hnsw
 pgvector_i <- resultsSorted[resultsSorted$Algorithm == 'pgvector_i',]
 pgvector_i
-plot(x=c(-0.1,1.1), y=c(10,10000), col="white" ,type = "b",pch=3,log="y")
+png(paste(path,"benchmark.png", sep = ""))
+plot(x=c(0.1,1.1), y=c(10,10000), col="white" ,type = "b",pch=3,log="y",xlab="Recall",ylab="Queries per second")
 
 lines(x=pgvector_hnsw$Recall, y=pgvector_hnsw$RecordsPerSecond, col="red" ,type = "b",pch=1)
 lines(x=pgvector_i$Recall, y=pgvector_i$RecordsPerSecond, col="green" ,type = "b",pch=4)
+legend(0.1, 100, legend=c("pgvector_hnsw", "pgvector_i"),  fill = c("red","green") )
+dev.off()
 
 
 
