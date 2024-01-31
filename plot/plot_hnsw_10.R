97fd93d9d2f0edc9b769e74c21e6d00bc1479d9d
Vladlen Popolitov <vladlenpopolitov@gmail.com>
2024-01-31 07:22:45 +0300

    changes

---
+++ plot/plot_hnsw_10.R
@@ -0,0 +1,79 @@
+library(stringr)
+library(tidyverse)
+#datasetName <- "lastfm"
+#datasetName <- "glove-100-a"
+#datasetName <- "fashion-mnist-784-e"
+#datasetName <- "galaxies-3-5000-e"
+#datasetName <- "galaxies-16-5000-e"
+datasetName <- "galaxies-16-1000000-e"
+queryLines <- "10"
+getwd()
+setwd(".")
+pathToRepoRoot <- "./Development/work/vectordb/vectordb"
+pathToResultsRoot <- paste(pathToRepoRoot,"results",sep="/")
+pathToAlgorithmRoot <- paste(pathToResultsRoot,datasetName,sep="/")
+path <- paste(pathToAlgorithmRoot,queryLines,sep="/")
+print( path )
+inputFile <- paste(path,"benchmark.csv", sep = "/")
+inputFile
+results <- read.csv(inputFile, sep = "\t")
+results
+resultsSorted = results[order(results$Dataset,results$Algorithm, results$Recall,results$RecordsPerSecond),]
+resultsSorted
+pgvector_hnsw <- resultsSorted[resultsSorted$Algorithm == 'pgvector_hnsw',]
+pgvector_hnsw
+pgvector_i <- resultsSorted[resultsSorted$Algorithm == 'pgvector_i',]
+pgvector_i
+embedding <- resultsSorted[resultsSorted$Algorithm == 'pg_embedding',]
+embedding
+
+substrRight <- function(x, n){substr(x, nchar(x)-n+1, nchar(x))}
+
+fc500<-filter(embedding, str_detect(embedding$Parameters,'fConstruction\'=>500,')==TRUE)
+arrange(fc500,desc(Recall))
+embeddingParm<-mutate(embedding,
+       efC=str_extract(str_extract(embedding$Parameters,',\'fConstruction\'=>[0-9]+,'),'[0-9]+'),
+       m=str_extract(str_extract(embedding$Parameters,',\'m\'=>[0-9]+'),'[0-9]+'),
+       eS=substrRight(paste("  ",str_extract(str_extract(embedding$Parameters,'\'eSearch\'=>[0-9]+,'),'[0-9]+')),3)
+       )
+embeddingParm
+embeddingParm<-mutate(embeddingParm,Legend=paste(paste('ef_const',embeddingParm@efC), paste('es',embeddingParm@eS)))
+
+ggplot(data = embeddingParm) + 
+  geom_point(mapping = aes(x = Recall, y = RecordsPerSecond,color=eS,shape=efC,size=m))
+
+ggplot(data = embeddingParm) + 
+  geom_point(mapping = aes(x = Recall, y = IndexTime,color=eS,shape=efC,size=m))
+
+lantern <- resultsSorted[resultsSorted$Algorithm == 'lantern',]
+lantern
+
+outputFile <- paste(path,"benchmark.png", sep = "/")
+png(outputFile)
+plot(x=c(0.05,1.1), y=c(1,60000), col="white" ,type = "b",pch=3,log="y",xlab="Recall",ylab="Queries per second")
+
+lines(x=pgvector_hnsw$Recall, y=pgvector_hnsw$RecordsPerSecond, col="red" ,type = "b",pch=1)
+lines(x=pgvector_i$Recall, y=pgvector_i$RecordsPerSecond, col="green" ,type = "b",pch=4)
+lines(x=embedding$Recall, y=embedding$RecordsPerSecond, col="blue" ,type = "b",pch=5)
+lines(x=lantern$Recall, y=lantern$RecordsPerSecond, col="brown" ,type = "b",pch=6)
+
+legend(0.06, 20, legend=c("pgvector_hnsw", "pgvector_i", "embedding_pg","lantern"),  
+       fill = c("red","green","blue","brown") )
+dev.off()
+
+outputFile <- paste(path,"benchmarkIndex.png", sep = "/")
+png(outputFile)
+plot(x=c(0.05,1.1), y=c(1,60000), col="white" ,type = "b",pch=3,log="y",xlab="Recall",ylab="Index creation time (seconds)")
+
+lines(x=pgvector_hnsw$Recall, y=pgvector_hnsw$IndexTime, col="red" ,type = "l",pch=1)
+lines(x=pgvector_i$Recall, y=pgvector_i$IndexTime, col="green" ,type = "l",pch=4)
+lines(x=embedding$Recall, y=embedding$IndexTime, col="blue" ,type = "l",pch=5)
+lines(x=lantern$Recall, y=lantern$IndexTime, col="brown" ,type = "b",pch=6)
+
+legend(0.06, 10000, legend=c("pgvector_hnsw", "pgvector_i", "embedding_pg","lantern"),  
+       fill = c("red","green","blue","brown") )
+dev.off()
+
+
+
+
